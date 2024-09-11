26ef2dcb846ac86f4edfaf8f9cae4ecbe48ac330
saransh-mathur <mathursaransh17@gmail.com>
2024-09-11 11:25:31 +0530

    Update Question.java

--- OOPJ/Assignments/Asg5/src/Question.java
+++ OOPJ/Assignments/Asg5/src/Question.java
@@ -1,19 +1,35 @@
-public class Question {
-    private static int instanceCount = 0;
+public class InstanceCounter {
 
-    public Question() {
+    private static int instanceCount = 0;
+    private String name;
+    static {
+        instanceCount = 0;  
+    }
+    public InstanceCounter(String name) {
+        this.name = name;
         instanceCount++;
     }
-
     public static int getInstanceCount() {
         return instanceCount;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return "InstanceCounter{name='" + name + "'}";
+    }
     public static void main(String[] args) {
-        new Question();
-        new Question();
-        new Question();
+        InstanceCounter obj1 = new InstanceCounter("Object1");
+        InstanceCounter obj2 = new InstanceCounter("Object2");
+        InstanceCounter obj3 = new InstanceCounter("Object3");
 
-        System.out.println("Number of instances created: " + Question.getInstanceCount());
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(obj3);
+
+        System.out.println("Total instances created: " + InstanceCounter.getInstanceCount());
     }
 }
